085015cee6969629b5e2fd1ed4a2b215db17b55d
Avishkar Deshmukh <138203333+davishkar@users.noreply.github.com>
2023-11-13 15:13:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Function to append a value to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to calculate the result
+function calculateResult() {
+    const expression = document.getElementById('display').value;
+
+    try {
+        const result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
